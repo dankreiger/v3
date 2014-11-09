305f1cea302d4672a2c0067dd5f2e6e28f665fe8
Dan Kreiger <dan@dankreiger.com>
2014-11-09 14:04:07 -0500

    server.rb

--- server.rb
+++ server.rb
@@ -6,3 +6,11 @@ get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
+get '/music' do
+  File.read(File.join('public', 'music.html'))
+end
+
+get '/contact' do
+  File.read(File.join('public', 'contact.html'))
+end
+
